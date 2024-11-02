09049a7d6f9cfeb4f5fe0aef43422a290a965a08
Brian <79370810+hearjest@users.noreply.github.com>
2024-11-02 13:42:58 -0400

    Fix Rendering bugs

--- my-app/src/app/home.js
+++ my-app/src/app/home.js
@@ -1,17 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import Map from "../Map";
 
 function Home() {
+  useEffect(() => {
+    // Load the Google Maps API script if it isn't already loaded
+    if (!document.querySelector("#google-maps-script")) {
+      const script = document.createElement("script");
+      script.id = "google-maps-script";
+      script.src = "https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY";
+      script.async = true;
+      document.body.appendChild(script);
+    }
+  }, []);
+
   return (
     <>
       <Head>
         <title>Add a Map using HTML</title>
-        <link rel="stylesheet" type="text/css" href="./style.css" />
+       
       </Head>
-      <main>
-        <Map />
-      </main>
+      <div
+        id="map"
+        style={{ height: '400px' }} // Correct usage of the style prop
+      >
+        <gmp-map
+          center="37.4220656,-122.0840897"
+          zoom="10"
+          map-id="DEMO_MAP_ID"
+        ></gmp-map>
+      </div>
     </>
   );
 }
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import Script from 'next/script';
 
 function Home() {
   return (
     <>
       <head>
         <title>Add a Map using HTML</title>
-
-        <link rel="stylesheet" type="text/css" href="./style.css" />
-        <script type="module" src="./index.js"></script>
       </head>
       <body>
         <gmp-map
           center="37.4220656,-122.0840897"
           zoom="10"
           map-id="DEMO_MAP_ID"
-          style="height: 400px"
+          style={{ height: '400px' }}
         ></gmp-map>
-        <script
+        <Script
           src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCA3yaFbVmsBYqh5O1R3Rd-6NEESD4TdbI&libraries=maps&v=beta"
-          defer
-        ></script>
+          strategy="lazyOnload"
+        />
+        <Script type="module" src="./index.js" strategy="lazyOnload" />
       </body>
     </>
   );
