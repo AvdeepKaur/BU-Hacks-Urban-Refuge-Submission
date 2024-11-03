5b6d8b8486fcd54693a4a4b51992c66590eea295
AvdeepKaur <avkaur@bu.edu>
2024-11-02 20:24:34 -0400

    added back in the logo

--- my-app/src/app/forumComponent.js
+++ my-app/src/app/forumComponent.js
@@ -52,6 +52,31 @@ export default function Forum({ onSearchSubmit, onBostonMarkers }) {
         borderRadius: "8px",
       }}
     >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+          paddingBottom: "15px",
+        }}
+      >
+        <img
+          src="/images/urban.png"
+          alt="MapRefuge Logo"
+          style={{ width: "100px", height: "auto" }} // Adjust the size as needed
+        />
+        <p
+          style={{
+            fontSize: "35px",
+            fontFamily: "Cocomat Pro",
+            fontWeight: "bold",
+            textTransform: "uppercase",
+            margin: "0 auto",
+          }}
+        >
+          Map Refuge
+        </p>
+      </div>
       <h3>Load Locations from CSV</h3>
       <form onSubmit={handleFormSubmit}>
         {error && (
